5245fa59c348ab17d192c7648fe456b04fe9a6de
daigokudo21-create <daigo.kudo21@gmail.com>
2026-03-16 14:26:44 +0900

    Update profit_calc.py

--- analysis/profit_calc.py
+++ analysis/profit_calc.py
@@ -1,10 +1,5 @@
-
-def calc_profit(buy,sell,repair):
-
-    fee=sell*0.1
-
-    shipping=300
-
-    profit=sell-buy-repair-fee-shipping
-
+def calc_profit(buy, sell, repair):
+    fee = sell * 0.10
+    shipping = 300
+    profit = sell - buy - repair - fee - shipping
     return int(profit)
